ae2e895543bfead566b07f5008b7de9a1151517b
dt2g23 <dt2g23@soton.ac.uk>
2024-08-04 16:05:05 +0100

    Installed react-router-dom, created jsx and css files for layout and worked on routes

--- aichat/src/main.jsx
+++ aichat/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/homepage/Homepage';
+import RootLayout from './layouts/rootLayout/RootLayout';
+// import DashboardPage from './routes/dashboard/DashboardPage';
+// import ChatPage from './routes/chatPage/ChatPage';
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path:"/", element: <Homepage />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ aichat/src/routes/signInPage/signInPage.jsx
@@ -0,0 +1,9 @@
+import './signInPage.css'
+
+const SignInPage = () => {
+    return (
+        <div className='signInPage'>SignUpPage</div>
+    )
+}
+
+export default SignInPage
