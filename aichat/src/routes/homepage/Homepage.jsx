f1f699c62c3e57d019efbda4bbb561821fef4359
dt2g23 <dt2g23@soton.ac.uk>
2024-08-05 17:33:18 +0100

    Left side of homepage completed, Get Started button, background (orbital) that moves.

--- aichat/src/routes/homepage/Homepage.jsx
+++ aichat/src/routes/homepage/Homepage.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom'
 import './homepage.css'
 
 const Homepage = () => {
     return (
-        <div className='homepage'>Homepage</div>
+        <div className='homepage'>
+            <img src="/orbital.png" alt="" className='orbital' />
+            <div className="left">
+                <h1>AI CHAT</h1>
+                <h2>Supercharge your creativity and productivity</h2>
+                <h3>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing
+                    elit. Ipsum sit modi veritatis rem esse ut.
+                </h3>
+                <Link to="/dashboard"></Link>
+                <button>Get Started</button>
+            </div>
+            <div className="right"></div>
+        </div>
     )
 }
 
