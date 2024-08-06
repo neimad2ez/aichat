2bdb15ee6a51a3a9e92be8c260c02f8da8cf7d94
dt2g23 <dt2g23@soton.ac.uk>
2024-08-06 18:40:58 +0100

    Added react-type-animation for homepage, completed homepage.

--- aichat/src/routes/homepage/Homepage.jsx
+++ aichat/src/routes/homepage/Homepage.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom'
 import './homepage.css'
+import { TypeAnimation } from 'react-type-animation'
+import { useState } from 'react';
 
 const Homepage = () => {
+
+const [typingStatus, setTypingStatus] = useState("human1");
+
     return (
         <div className='homepage'>
-            <img src="/orbital.png" alt="" className='orbital' />
+            <img src="orbital.png" alt="" className='orbital' />
             <div className="left">
                 <h1>AI CHAT</h1>
                 <h2>Supercharge your creativity and productivity</h2>
@@ -12,10 +17,58 @@ const Homepage = () => {
                     Lorem, ipsum dolor sit amet consectetur adipisicing
                     elit. Ipsum sit modi veritatis rem esse ut.
                 </h3>
-                <Link to="/dashboard"></Link>
-                <button>Get Started</button>
+                <Link to="/dashboard">Get Started</Link>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="imgContainer">
+                    <div className="bgContainer">
+                        <div className="bg"></div>
+                    </div>
+                    <img src="/bot.png" alt="" className="bot" />
+                    <div className="chat">
+                        <img src={typingStatus === "human1"
+                            ? "/human1.jpeg"
+                            : typingStatus === "human2"
+                            ? "/human2.jpeg"
+                            : "bot.png"
+                            }
+                            alt="" />
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Human1: We produce food for Mice',
+                                2000, () => {
+                                    setTypingStatus("bot");
+                                },
+                                'Bot: We produce food for Hamsters',
+                                2000, () => {
+                                    setTypingStatus("human2");
+                                },
+                                'Human2: We produce food for Guinea Pigs',
+                                2000, () => {
+                                    setTypingStatus("bot");
+                                },
+                                'Bot: We produce food for Chinchillas',
+                                2000, () => {
+                                    setTypingStatus("human1");
+                                }
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            cursor={true}
+                            repeat={Infinity}
+                            omitDeletionAnimation={true}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="terms">
+                <img src="/logo.png" alt="" />
+                <div className="links">
+                    <Link to="/">Terms of Service</Link>
+                    <Link to="/">Privacy Policy</Link>
+                </div>
+            </div>
         </div>
     )
 }
