7f47a5f3bb30aeafee540b110836ee43eaa5778e
dt2g23 <dt2g23@soton.ac.uk>
2024-08-09 23:58:22 +0100

    AI response now outputs line by line rather than all at once.

--- aichat/client/src/components/newPrompt/NewPrompt.jsx
+++ aichat/client/src/components/newPrompt/NewPrompt.jsx
@@ -5,14 +5,31 @@ import { IKImage } from 'imagekitio-react';
 import model from "../../lib/gemini"
 import Markdown from "react-markdown"
 
-const NewPrompt = () => {
+const NewPrompt = () => {   
     const [question, setQuestion] = useState("")
     const [answer, setAnswer] = useState("")
     const [img, setImg] = useState({
         isLoading:false,
         error:"",
-        dbData:{}
+        dbData:{},
+        aiData: {},
     })
+
+    const chat = model.startChat({
+        history: [
+          {
+            role: "user",
+            parts: [{ text: "Hello, I have 2 dogs in my house." }],
+          },
+          {
+            role: "model",
+            parts: [{ text: "Great to meet you. What would you like to know?" }],
+          },
+        ],
+        generationConfig: {
+        //   maxOutputTokens: 100,
+        },
+      });
 
     const endRef = useRef(null);
 
@@ -23,9 +40,22 @@ const NewPrompt = () => {
     const add = async (text) => {
         setQuestion(text)
       
-        const result = await model.generateContent(text);
-        const response = await result.response;
-        setAnswer(response.text())
+        const result = await chat.sendMessageStream(
+            Object.entries(img.aiData).length ? [img.aiData, text] : [text]
+        );
+        let accumulatedText = "";
+        for await (const chunk of result.stream) {
+            const chunkText = chunk.text();
+            console.log(chunkText);
+            accumulatedText += chunkText;
+            setAnswer(accumulatedText)
+        }
+        setImg({
+            isLoading:false,
+            error:"",
+            dbData:{},
+            aiData: {},
+        })
     }
 
     const handleSubmit = async (e) => {
