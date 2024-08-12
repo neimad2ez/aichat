cee181c544024c713248c53ccbc03e49fd25a946
dt2g23 <dt2g23@soton.ac.uk>
2024-08-12 03:52:11 +0100

    Installed Clerk SDK, can retrieve userID from Clerk for verification. User can only access chat if verified user.

--- aichat/backend/index.js
+++ aichat/backend/index.js
@@ -4,12 +4,14 @@ import ImageKit from "imagekit";
 import mongoose from "mongoose";
 import Chat from "./models/chat.js";
 import UserChats from "./models/userChats.js";
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
+    credentials:true,
 }));
 
 app.use(express.json())
@@ -34,8 +36,19 @@ app.get("/api/upload", (req, res) => {
     res.send(result)
 })
 
-app.post("/api/chats", async (req, res) => {
-    const {userId, text} = req.body
+// app.get("/api/test", ClerkExpressRequireAuth(), (req, res) => {
+//     const userId = req.auth.userId;
+//     console.log(userId);
+//     res.send("Success!")
+// })
+
+app.post("/api/chats", 
+    ClerkExpressRequireAuth({
+        
+    }),
+    async (req, res) => {
+    const userId = req.auth.userId;
+    const {text} = req.body
     try {
         //Create a new chat
         const newChat = new Chat({
@@ -80,6 +93,11 @@ app.post("/api/chats", async (req, res) => {
         res.status(500).send("Error creating chat!")
     }
 })
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(401).send('Unauthenticated!');
+  });
 
 app.listen(port, () => {
     connect()
